f2f9c2a4bf673231f2378322ec0b3d580c9bcb99
uskay <uskay@uskay-macbookpro.roam.corp.google.com>
2018-02-12 16:44:57 +0100

    Fixing 001-pwa-blog-loading.md

--- functions/BuildVersion.js
+++ functions/BuildVersion.js
@@ -2,7 +2,7 @@
             /** Auto generated module */
             class BuildVersion {
                 getVersion() {
-                    return 'v1518447395400';
+                    return 'v1518450174387';
                 }
             }
             module.exports = BuildVersion;
