7253c5599744f19931fbdcf1328f2c2fc72cbb9f
uskay <uskay@google.com>
2019-11-19 13:08:11 +0900

    Fix typo

--- functions/BuildVersion.js
+++ functions/BuildVersion.js
@@ -2,7 +2,7 @@
             /** Auto generated module */
             class BuildVersion {
                 getVersion() {
-                    return 'v1574112775481';
+                    return 'v1574136440154';
                 }
             }
             module.exports = BuildVersion;
