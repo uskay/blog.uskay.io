93978aa37e988bae8e189db62c2b50cbc6cf08fe
uskay <uskay@google.com>
2020-06-17 11:02:05 +0900

    update self photo

--- functions/BuildVersion.js
+++ functions/BuildVersion.js
@@ -2,7 +2,7 @@
             /** Auto generated module */
             class BuildVersion {
                 getVersion() {
-                    return 'v1592358301415';
+                    return 'v1592359251362';
                 }
             }
             module.exports = BuildVersion;
